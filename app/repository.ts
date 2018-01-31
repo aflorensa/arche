d973a51eae012df3732a4cc41cdaf36b028898e5
antoni <aflorensamoyano@gmail.com>
2018-01-31 11:19:56 +0100

    first commit

--- app/repository.ts
+++ app/repository.ts
@@ -21,7 +21,7 @@ export class Repository {
 
     public clone(): string {
 
-        let  command = "cd "+this.rule.destination+" && git clone " + this.rule.repository.url + " " + this.rule.name;
+        let  command = "cd "+this.rule.destination+" && git clone --no-hardlinks" + this.rule.repository.url + " " + this.rule.name;
         if (shell.exec(command).code == 0) {
                  // stuff here
         }
@@ -46,14 +46,18 @@ export class Repository {
     public add(){
 
 
-            let  command1 = "git filter-branch --prune-empty --subdirectory-filter dist/app/"+ this.rule.name+ " master";
+            let  command1 = "git subtree split -P dist/app/"+ this.rule.name+ " -b master";
             if (shell.exec(command1).code == 0) {
                 // stuff here
             }
             shell.cd(this.projectPath)
-            let  command2 = "git remote add origin2 master"+ this.rule.push + this.rule.name +".git && git push -u origin2 master";
+            let  command2 = "git init && git add -A && git commit -m 'first commit'";
             if (shell.exec(command2).code == 0) {
                 // stuff here
+            }
+            let command3 = "git remote add origin master"+ this.rule.push + this.rule.name +".git && git push -u origin master";
+            if (shell.exec(command3).code == 0) {
+                // stuff here\n" +
             }
         }
 }
